0b8838707ce8d1b55ba3191b892fe714da6c0342
Marco <you@example.com>
2026-05-13 16:16:59 +0200

    test4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: true
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['cineconnect.octilabs.com']
+  }
 })
